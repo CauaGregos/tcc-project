f9bf90937532cf80cce02529565cf3af2eeb02c9
CauaGregos <cauanunes013@gmail.com>
2022-08-24 19:44:41 -0300

    fix2408

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Routes from './src/routes/Routes';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+
 export default function App() {
-  return (  <NavigationContainer>
-            <StatusBar backgroundColor='#202020'/>
-            <Routes/>
-            </NavigationContainer>);
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor='#202020' />
+      <Routes />
+    </NavigationContainer>
+  );
 }
 
 
--- api/config/bdConfig.js
+++ api/config/bdConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    // host: '127.0.0.1',
-    // user: 'root',
-    // password: '',
-    // database: 'enderecoalunos'
+    // host: 'us-cdbr-east-06.cleardb.net',
+    // user: 'b17ab4e5b7c12b',
+    // password: '0545a23c',
+    // database: 'heroku_1e8f08e81112628'
     host: process.env.NODE_MYSQL_SERVIDOR,
     user: process.env.NODE_MYSQL_USUARIO,
     password: process.env.NODE_MYSQL_SENHA,
