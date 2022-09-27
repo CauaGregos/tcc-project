890616f7fe5cf3695e148a9c1af44af0c008bab6
Rafael Zanon <rafael.zikazanon@gmail.com>
2022-09-26 22:04:37 -0300

    fix2609

--- src/pages/GameScreens/earthGame.js
+++ src/pages/GameScreens/earthGame.js
@@ -4,7 +4,9 @@ import {
     Platform,
     Image,
     View,
-    Button
+    Button,
+    TouchableOpacity,
+    Text
 } from "react-native";
 import { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -44,12 +46,31 @@ const earthGame = (props) => {
                 <ScrollView>
                     <Image source={require('../assets/earthGame.png')}
                         style={styles.IOSearthGame}></Image>
+                        
                 </ScrollView>
             ) : (
                 <ScrollView>
                     <Image source={require('../assets/earthGame.png')}
-                        style={styles.ANDROIDearthGame}
-                    ></Image>
+                        style={styles.ANDROIDearthGame}></Image>
+                    <TouchableOpacity style={{top: "-20%" , fontColor: "#000", width: 10, alignSelf:'center'}}>
+                            <Text>Botao</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{top: "-33.6%" , right:60 ,fontColor: "#000", width: 10, alignSelf:'center'}}>
+                            <Text>Botao</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{top: "-40%" , left:60 ,fontColor: "#000", width: 10, alignSelf:'center'}}>
+                            <Text>Botao</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{top: "-52%" , right:0 ,fontColor: "#000", width: 10, alignSelf:'center'}}>
+                            <Text>Botao</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{top: "-65%" , right:60 ,fontColor: "#000", width: 10, alignSelf:'center'}}>
+                            <Text>Botao</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{top: "-65%" , left:60 ,fontColor: "#000", width: 10, alignSelf:'center'}}>
+                            <Text>Botao</Text>
+                    </TouchableOpacity>
+                    
                 </ScrollView>
             )
             }</View>
