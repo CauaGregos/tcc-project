ae9183c102b43e1e447d197dd7d2bb60233c5275
Rafael Zanon <rafael.zikazanon@gmail.com>
2022-10-10 20:16:23 -0300

    fix10/10BUTTON

--- src/pages/GameScreens/styleAndroid.js
+++ src/pages/GameScreens/styleAndroid.js
@@ -2,16 +2,16 @@ import React from "react";
 import { StyleSheet, Dimensions } from "react-native";
 
 const stylesAndroid = StyleSheet.create({
-  ButtonOne: {top: "6.4%",right: "56%",width: 10,alignSelf: "center",position: "absolute" },
-  ButtonTwo: {top: "18.6%",right:"41%",width: 10, alignSelf: "center",position: "absolute"},
-  ButtonThree: {top: "18.6%",right:"70.6%",width: 10, alignSelf: "center",position: "absolute"},
-  ButtonFour: {top: "30.9%",right:"56%",width: 10,alignSelf: "center",position: "absolute"},
-  ButtonFive: { top: "43.2%",right:"41%", width: 10, alignSelf: "center" ,position: "absolute"},
-  ButtonSix: { top: "43.2%",right:"70.6%", width: 10, alignSelf: "center" ,position: "absolute"},
-  ButtonSeven: { top: "55.5%",right:"56%", width: 10, alignSelf: "center" ,position: "absolute"},
-  ButtonEight: { top: "67.9%",right:"41%", width: 10, alignSelf: "center" ,position: "absolute"},
-  ButtonNine: { top: "67.9%",right:"70.6%", width: 10, alignSelf: "center",position: "absolute"},
-  ButtonTen: { top: "80.5%",right:"56%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonOne: {top: 90,right: "56.5%",width: 10,alignSelf: "center",position: "absolute" },
+  ButtonTwo: {top: 213,right:"41.5%",width: 10, alignSelf: "center",position: "absolute"},
+  ButtonThree: {top: 213,right:"71%",width: 10, alignSelf: "center",position: "absolute"},
+  ButtonFour: {top: 337,right:"56.5%",width: 10,alignSelf: "center",position: "absolute"},
+  ButtonFive: { top: 463,right:"41.5%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonSix: { top: 463,right:"71%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonSeven: { top: 588,right:"56.5%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonEight: { top: 713,right:"41.5%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonNine: { top: 713,right:"71%", width: 10, alignSelf: "center",position: "absolute"},
+  ButtonTen: { top: 843,right:"56.5%", width: 10, alignSelf: "center" ,position: "absolute"},
 });
 
 
--- src/pages/GameScreens/styleIOS.js
+++ src/pages/GameScreens/styleIOS.js
@@ -2,16 +2,16 @@ import React from "react";
 import { StyleSheet, Dimensions } from "react-native";
 
 const stylesIOS = StyleSheet.create({
-  ButtonOne: {top: "6.4%",right: "56%",width: 10,alignSelf: "center",position: "absolute" },
-  ButtonTwo: {top: "18.6%",right:"41%",width: 10, alignSelf: "center",position: "absolute"},
-  ButtonThree: {top: "18.6%",right:"70.6%",width: 10, alignSelf: "center",position: "absolute"},
-  ButtonFour: {top: "30.9%",right:"56%",width: 10,alignSelf: "center",position: "absolute"},
-  ButtonFive: { top: "43.2%",right:"41%", width: 10, alignSelf: "center" ,position: "absolute"},
-  ButtonSix: { top: "43.2%",right:"70.6%", width: 10, alignSelf: "center" ,position: "absolute"},
-  ButtonSeven: { top: "55.5%",right:"56%", width: 10, alignSelf: "center" ,position: "absolute"},
-  ButtonEight: { top: "67.9%",right:"41%", width: 10, alignSelf: "center" ,position: "absolute"},
-  ButtonNine: { top: "67.9%",right:"70.6%", width: 10, alignSelf: "center",position: "absolute"},
-  ButtonTen: { top: "80.5%",right:"56%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonOne: {top: 69,right: "56%",width: 10,alignSelf: "center",position: "absolute" },
+  ButtonTwo: {top: 197,right:"41.2%",width: 10, alignSelf: "center",position: "absolute"},
+  ButtonThree: {top: 197,right:"70.7%",width: 10, alignSelf: "center",position: "absolute"},
+  ButtonFour: {top: 329,right:"56%",width: 10,alignSelf: "center",position: "absolute"},
+  ButtonFive: { top: 460,right:"41.2%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonSix: { top: 460,right:"70.7%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonSeven: { top: 592,right:"56%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonEight: { top: 724,right:"41.2%", width: 10, alignSelf: "center" ,position: "absolute"},
+  ButtonNine: { top: 724,right:"70.7%", width: 10, alignSelf: "center",position: "absolute"},
+  ButtonTen: { top: 860,right:"56%", width: 10, alignSelf: "center" ,position: "absolute"},
 });
 
 export default stylesIOS;
