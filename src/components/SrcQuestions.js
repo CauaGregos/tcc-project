bd4de4b2faa3487719ff64e47b92548bf91125a6
rafaelZanon <rafael.zikazanon@gmail.com>
2022-10-14 10:17:48 -0300

    fix14/10ExerciciosMars

--- src/components/SrcQuestions.js
+++ src/components/SrcQuestions.js
@@ -316,6 +316,292 @@ const SourceQuestions = (props, planet) => {
             reqProgres: 100
         },],
         'mars':[
+            { //fase 32
+                question: '* Meu pai está com fome * em Inglês é?',
+                resp:  'My Father is hungry',
+
+                hasOptions: true,
+                op1: 'My Mom is hungry',
+                op2: 'My Boyfriend is hungry',
+                op3: 'My Father is hungry',
+                op4: 'My Dog is hungry',
+                reqProgres:  0
+            },
+            { //fase 33
+                question: 'Traduza *My Mom is cooking* pro Português \n OBS: Não esqueça dos acentos',
+                resp:  'Minha mãe está cozinhando',
+
+                hasOptions: false,
+                reqProgres:  0
+            },
+            { //fase 34
+                question: '* Eu quero jogar futebol * em Inglês é?',
+                resp:  'I want to play Soccer',
+
+                hasOptions: true,
+                op1: 'I want to play Video-games',
+                op2: 'I want to play Soccer',
+                op3: 'I want to play Basket',
+                op4: 'I want to play computer',
+                reqProgres:  0
+            },
+            { //fase 35
+                question: 'Escreva em Inglês : *  Tenha uma boa noite, senhora Maria *',
+                resp:  'Have a nice night, Mrs Maria',
+
+                hasOptions: false,
+                reqProgres:  10
+            },
+            { //fase 36
+                question: '* Tenha uma boa noite * em Inglês é?',
+                resp:  'Have a nice night',
+
+                hasOptions: true,
+                op1: 'Have a good evening',
+                op2: 'Have a good day',
+                op3: 'Have a good afternoon',
+                op4: 'Have a nice night',
+                reqProgres:  10
+            },
+            { //fase 37
+                question: '* Eu quero comer chocolate * em Inglês é? ',
+                resp:  'I want to eat chocolate',
+
+                hasOptions: true,
+                op1: 'I want to eat candy',
+                op2: 'I want to eat rice',
+                op3: 'I want to eat chocolate',
+                op4: 'I want to eat beans',
+                reqProgres:  10
+            },
+            { //fase 38
+                question: '* Eu gosto de doce * em Inglês é?',
+                resp:  'I like candy',
+
+                hasOptions: true,
+                op1: 'I like candy',
+                op2: 'I like salt things',
+                op3: 'I like water',
+                op4: 'I like ice cream',
+                reqProgres:  20
+            },
+            { //fase 39
+                question: ' Traduza * Hoje é um dia quente * para o Inglês',
+                resp:  'Today is a hot day',
+
+                hasOptions: false,
+                reqProgres:  20
+            },
+            { //fase 40
+                question: ' * I like programming * em Português é?',
+                resp:  'Eu gosto de programar',
+
+                hasOptions: true,
+                op1: 'Eu gosto de comer',
+                op2: 'Eu gosto de programar',
+                op3: 'Eu gosto de correr',
+                op4: 'Eu gosto de assistir Tv',
+                reqProgres:  20
+            },
+            { //fase 41
+                question: ' * React native é minha linguagem de programação preferida * em Inglês é?',
+                resp:  'React native is my favorite programming language',
+
+                hasOptions: true,
+                op1: 'I hate React native',
+                op2: 'React native is the best programming language',
+                op3: 'React native is the worse programming language',
+                op4: 'React native is my favorite programming language',
+                reqProgres:  30
+            },
+            { //fase 42
+                question: ' * Whats your favorite band? * Em Português é?',
+                resp:  'Qual sua banda favorita?',
+
+                hasOptions: true,
+                op1: 'Qual sua banda favorita?',
+                op2: 'Qual sua música favorita?',
+                op3: 'Qual sua comida favorita?',
+                op4: 'Qual sua bebida favorita?',
+                reqProgres:  30
+            },
+            { //fase 43
+                question: ' * Qual é a sua comida favorita? * em Inglês é?',
+                resp:  'Whats your favorite food',
+
+                hasOptions: true,
+                op1: 'Whats your favorite drink?',
+                op2: 'Whats your favorite food?',
+                op3: 'Whats your favorite game?',
+                op4: 'Whats your favorite sport?',
+                reqProgres:  30
+            },
+            { //fase 44
+                question: ' * Notebook * em Português é?',
+                resp:  'Caderno',
+
+                hasOptions: true,
+                op1: 'Computador',
+                op2: 'Notebook',
+                op3: 'Caderno',
+                op4: 'Bloco de notas',
+                reqProgres:  40
+            },
+            { //fase 45
+                question: 'Escreva em Inglês: * Meu pai ama a minha mãe *',
+                resp:  'My father loves my mother',
+
+                hasOptions: false,
+                reqProgres:  40
+            },
+            { //fase 46
+                question: 'Escreva em Inglês: * Minha mãe é casada com o meu pai *',
+                resp:  'My mother is married with my father',
+
+                hasOptions: false,
+                reqProgres:  40
+            },
+            { //fase 47
+                question: ' * Guitar * em Inglês é?',
+                resp:  'Violao',
+
+                hasOptions: true,
+                op1: 'Violao',
+                op2: 'Guitarra',
+                op3: 'Baixo',
+                op4: 'Bateria',
+                reqProgres:  50
+            },
+            { //fase 48
+                question: 'Traduza para o Português : * I like to play guitar *',
+                resp:  'Eu gosto de tocar violao',
+
+                hasOptions: false,
+                reqProgres:  50
+            },
+            { //fase 49
+                question: ' * Eletric Guitar * em Inglês é?',
+                resp:  'Guitarra',
+
+                hasOptions: true,
+                op1: 'Violão Elétrico',
+                op2: 'Guitarra',
+                op3: 'Baixo',
+                op4: 'Piano',
+                reqProgres:  50
+            },
+            { //fase 50
+                question: 'Escreva em Português: * I have a very good keyboard *',
+                resp:  'Eu tenho um teclado muito bom',
+
+                hasOptions: false,
+                reqProgres:  60
+            },
+            { //fase 51
+                question: '* Eu gostaria de ter um computador gamer * em Inglês é?',
+                resp:  'I would like to have a gaming computer',
+
+                hasOptions: true,
+                op1: 'I would like to have a gaming headset',
+                op2: 'I would like to have a gaming Mouse',
+                op3: 'I would like to have a gaming smartPhone',
+                op4: 'I would like to have a gaming computer',
+                reqProgres:  60
+            },
+            { //fase 52
+                question: 'Escreva em Inglês: * Eu gosto de jogar jogos * ',
+                resp:  'I like to play games',
+
+                hasOptions: false,
+                reqProgres:  60
+            },
+            { //fase 53
+                question: 'Escreva em Inglês: * Eu preciso correr *',
+                resp:  'I need to run',
+
+                hasOptions: false,
+                reqProgres:  70
+            },
+            { //fase 54
+                question: 'Escreva em Inglês: * Eu preciso comer *',
+                resp:  'I need to eat',
+
+                hasOptions: false,
+                reqProgres:  70
+            },
+            { //fase 55
+                question: ' * Eu estudo desenvolvimento de sistemas * Em inglês é? *',
+                resp:  'I study Development Systems',
+
+                hasOptions: true,
+                op1: 'I study Development Systems',
+                op2: 'I study Systems',
+                op3: 'Im a Developer',
+                op4: 'I study how to program',
+                reqProgres:  70
+            },
+            { //fase 56
+                question: ' * Eu gosto de assistir videos * em Inglês é?',
+                resp:  'I like to watch videos',
+
+                hasOptions: true,
+                op1: 'I like to watch TV',
+                op2: 'I like to watch sports',
+                op3: 'I like to watch videos',
+                op4: 'I like to watch music shows',
+                reqProgres:  80
+            },
+            { //fase 57
+                question: ' * I want to buy a car * em Português é?',
+                resp:  'Eu quero comprar um carro',
+
+                hasOptions: true,
+                op1: 'Eu quero comprar uma moto',
+                op2: 'Eu quero comprar uma van',
+                op3: 'Eu quero comprar um tank',
+                op4: 'Eu quero comprar um carro',
+                reqProgres:  80
+            },
+            { //fase 58
+                question: '* I want to buy a headset * em Português é?',
+                resp:  'Eu quero comprar um fone',
+
+                hasOptions: true,
+                op1: 'Eu quero comprar um fone',
+                op2: 'Eu quero comprar um cachorro',
+                op3: 'Eu quero comprar um mouse',
+                op4: 'Eu quero comprar um computador',
+                reqProgres:  80
+            },
+            { //fase 59
+                question: ' * Minha fruta favorita é Morango *  em Inglês é?',
+                resp:  'My favorite fruit is Strawberry',
+
+                hasOptions: true,
+                op1: 'My favorite fruit is Banana',
+                op2: 'My favorite fruit is Strawberry',
+                op3: 'My favorite fruit is Apple',
+                op4: 'My favorite fruit is Grape',
+                reqProgres:  90
+            },
+            { //fase 60
+                question: '* Minha fruta favorita é Uva *  em Inglês é?',
+                resp:  'My favorite fruit is Grape',
+
+                hasOptions: true,
+                op1: 'My favorite fruit is Grape',
+                op2: 'My favorite fruit is Apple',
+                op3: 'My favorite fruit is Pear',
+                op4: 'My favorite fruit is Kiwi',
+                reqProgres:  90
+            },
+            { //fase 61
+                question: 'Escreva: * Morango * Em Inglês:',
+                resp:  'Strawberry',
+
+                hasOptions: false,
+                reqProgres:  100
+            },
 
         ]  
     }
